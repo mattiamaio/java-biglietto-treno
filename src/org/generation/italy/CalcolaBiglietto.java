589a4b188b3577a4fe8c7af377bba5edf93ce8b4
Mattia Maio <mattia.maio10@gmail.com>
2021-11-16 15:10:26 +0100

    Initial commit

---
+++ src/org/generation/italy/CalcolaBiglietto.java
@@ -0,0 +1,48 @@
+package org.generation.italy;
+import java.util.Scanner;
+
+public class CalcolaBiglietto {
+
+	public static void main(String[] args) {
+		
+		//init scanner
+		Scanner input = new Scanner(System.in);
+		
+		//init variables
+		double distance;
+		int age;
+		double pricePKm = 0.21D;
+		
+		//greeting
+		System.out.println("Good morning!");
+		
+		//scan user data
+		System.out.print("Please enter travel distance: ");
+		distance = input.nextDouble();
+		
+		System.out.print("Please enter your age: ");
+		age = input.nextInt();
+		
+		double ticketPrice = distance * pricePKm;
+		
+		//conditions
+		if (age < 18) {
+			ticketPrice = (ticketPrice - ((ticketPrice * 20) / 100));
+			System.out.println(("Your ticket fare is: ") + ticketPrice + "\u20AC");
+		
+		} else if (age >= 65) {
+			ticketPrice = (ticketPrice - ((ticketPrice * 40) / 100));
+			System.out.println(("Your ticket fare is: ") + ticketPrice + "\u20AC");
+		
+		} else {
+			System.out.println(("Your ticket fare is: ") + ticketPrice + "\u20AC");
+		}
+			
+		System.out.println("Thank you for traveling with us!");
+		
+		
+		input.close();
+		
+	}
+
+}
